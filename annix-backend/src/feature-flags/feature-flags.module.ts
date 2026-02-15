3628722d9badc89bf5a2644ee7fa703453b05ca5
nbarrett <nick.barrett36@me.com>
2026-02-15 16:35:52 +0200

    fix: resolve circular dependency in FeatureFlagsModule (ref #67)
    Use forwardRef for AdminModule import in FeatureFlagsModule to break
    circular dependency chain: CustomerModule -> FeatureFlagsModule ->
    AdminModule -> RfqModule -> CustomerModule

--- annix-backend/src/feature-flags/feature-flags.module.ts
+++ annix-backend/src/feature-flags/feature-flags.module.ts
@@ -1,4 +1,4 @@
-import { Module } from "@nestjs/common";
+import { forwardRef, Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AdminModule } from "../admin/admin.module";
@@ -7,7 +7,7 @@ import { FeatureFlagsController } from "./feature-flags.controller";
 import { FeatureFlagsService } from "./feature-flags.service";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([FeatureFlag]), ConfigModule, AdminModule],
+  imports: [TypeOrmModule.forFeature([FeatureFlag]), ConfigModule, forwardRef(() => AdminModule)],
   providers: [FeatureFlagsService],
   controllers: [FeatureFlagsController],
   exports: [FeatureFlagsService],
